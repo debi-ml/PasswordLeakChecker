83d98d21d629d4a7c5e7b9aa440c73c14e52c63a
Matej <matej.loris1@gmail.com>
2023-10-05 17:22:09 +0200

    Added feature: GUI password check from .txt file

--- src/pwcheck.py
+++ src/pwcheck.py
@@ -2,6 +2,8 @@ import requests
 import hashlib
 import sys
 import os
+import tkinter as tk
+from tkinter import messagebox
 
 # Takes first 5 characters of SHA1 hash of the password and sends a request to api, returns response
 def request_api_data(input_char):
@@ -20,41 +22,37 @@ def get_api_password_hashes(password):
 
 # Takes hashes from api, and the tail of sha1 of password, outputs the number of times pw was pwned
 def get_api_password_leaks_count(hashes, hash_to_check):
-
     hashes = (line.split(":") for line in hashes.text.splitlines())
     for h, count in hashes:
         if h == hash_to_check:
             return count
     return 0
 
-
 # Gets password from password.txt 
 def get_text_password():
-
     password_text_path = os.path.join(os.getcwd(), "password.txt")
+    if not os.path.exists(password_text_path):
+        open(password_text_path, 'w').close()
+        os.startfile(password_text_path)
+        sys.exit(0)
 
     with open(password_text_path, 'r') as file:
         password_text = file.read()
 
     return password_text
 
-
-# Takes argv and text file for passwords, and
+# Takes argv and text file
 def main():
-    #
     argv = sys.argv[1:]
     text = get_text_password()
 
     if argv:
         for item in argv:
             print(get_api_password_hashes(item))
+        sys.exit(0)
+    
     if text:
         print(get_api_password_hashes(text))
-    
-#main()
-
-import tkinter as tk
-from tkinter import messagebox
 
 def check_password():
     password = password_entry.get()
@@ -64,6 +62,12 @@ def check_password():
     else:
         messagebox.showerror("Error", "Please enter a password.")
 
+def check_file_password():
+    text = get_text_password()
+    if text:
+        result = get_api_password_hashes(text)
+        result_text.set(f"Your password was found {result} times in pwned passwords database.")
+        
 root = tk.Tk()
 
 password_text = tk.StringVar()
@@ -73,8 +77,14 @@ password_entry.pack()
 check_button = tk.Button(root, text="Check Password", command=check_password)
 check_button.pack()
 
+check_file_button = tk.Button(root, text="Check File Password", command=check_file_password)
+check_file_button.pack()
+
 result_text = tk.StringVar()
 result_label = tk.Label(root, textvariable=result_text)
 result_label.pack()
 
 root.mainloop()
+
+if __name__ == "__main__":
+    main()
